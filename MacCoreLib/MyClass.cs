4d5e8a584dc1f4db70399a4038669189737bbc58
Ilhwan Shin <acoross@gmail.com>
2017-01-08 01:59:11 +0900

    Add more projects from mac.

---
+++ MacCoreLib/MyClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MacCoreLib
+{
+    public class MyClass
+    {
+        public MyClass()
+        {
+        }
+    }
+}
---
+++ MacPCL/MyClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace MacPCL
+{
+    public class MyClass
+    {
+        public MyClass()
+        {
+        }
+    }
+}
